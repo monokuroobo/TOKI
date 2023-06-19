95d149399fed91044fdeded4713adc6a7a38a6d0
monokuroobo <136736689+monokuroobo@users.noreply.github.com>
2023-06-20 00:25:52 +0700

    Add files via upload

---
+++ Percabangan/ifThenElse.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int x;
+	cin >> x;
+	if (x > 0){
+		cout << "positif";
+	}
+	else if (x < 0){
+		cout << "negatif";
+	}
+	else{
+		cout << "nol";
+	}
+}
